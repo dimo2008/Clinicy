e182f8e910674108e1c73dded5239838321b0b0e
dimo2008 <bio.eng.ahmed.magdy@gmail.com>
2026-05-03 21:02:02 +0300

    create angular first commit

--- Frontend/src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [
-  { path: '', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) },
-  { path: 'patients', loadChildren: () => import('./patient/patient.module').then(m => m.PatientModule) },
-];
--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
+  standalone: false,
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Frontend');
+  protected readonly title = signal('Ruba Clinic');
 }
